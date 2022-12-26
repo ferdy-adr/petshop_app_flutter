ac9c755aeb807dc036261c860e5fda7b2d8bd90b
Ferdy Adriantama <ferdypbg@gmail.com>
2022-12-26 22:13:50 +0700

    make Splash Screen

---
+++ lib/ui/onboarding.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:petshop_app_flutter/ui/style_guide.dart';
+
+final List<String> imgList = [
+  'assets/images/illustration1.png',
+  'assets/images/illustration2.png',
+  'assets/images/illustration3.png',
+];
+
+final List<Widget> imageSliders = imgList
+    .map((item) => Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20),
+          child: Image.asset(
+            item,
+          ),
+        ))
+    .toList();
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: offWhite,
+      body: SafeArea(
+        child: Row(
+          children: [
+            Expanded(
+              child: Stack(
+                children: [
+                  SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(24, 36, 24, 0),
+                          child: const Text(
+                            'Meet your animal needs here',
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w600,
+                              fontSize: 40,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                        const Carousel(),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 24),
+                    child: Align(
+                      alignment: const Alignment(1, 0.92),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: SizedBox(
+                              height: 60,
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                style: ButtonStyle(
+                                  elevation: const MaterialStatePropertyAll(0),
+                                  shape: MaterialStatePropertyAll(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(30),
+                                    ),
+                                  ),
+                                  backgroundColor:
+                                      const MaterialStatePropertyAll(
+                                          mainColor100),
+                                ),
+                                child: const Text(
+                                  'Get Started',
+                                  style: TextStyle(
+                                    fontFamily: 'Poppins',
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w700,
+                                    color: offWhite,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Carousel extends StatefulWidget {
+  const Carousel({super.key});
+
+  @override
+  State<Carousel> createState() => _CarouselState();
+}
+
+class _CarouselState extends State<Carousel> {
+  int _current = 0;
+  final CarouselController _controller = CarouselController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: CarouselSlider(
+                items: imageSliders,
+                carouselController: _controller,
+                options: CarouselOptions(
+                  viewportFraction: 1,
+                  aspectRatio: 4 / 3,
+                  enableInfiniteScroll: false,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      _current = index;
+                    });
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+        Container(
+          margin: const EdgeInsets.symmetric(vertical: 18, horizontal: 24),
+          child: const Text(
+            'Get interesting promos here, register your account immediately so you can meet your animal needs.',
+            textAlign: TextAlign.justify,
+            style: TextStyle(
+              fontFamily: 'Poppins',
+              fontSize: 14,
+              color: Color.fromRGBO(137, 137, 137, 1.0),
+            ),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 24),
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: imgList.asMap().entries.map((entry) {
+                return GestureDetector(
+                  onTap: () => _controller.animateToPage(entry.key),
+                  child: Container(
+                    width: 8.0,
+                    height: 8.0,
+                    margin: const EdgeInsets.symmetric(
+                      vertical: 6.0,
+                      horizontal: 4.0,
+                    ),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color:
+                          (_current == entry.key) ? mainColor100 : blackWhite10,
+                    ),
+                  ),
+                );
+              }).toList()),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/ui/style_guide.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+// --------------
+// Color Pallette
+
+// Main Color
+const Color mainColor100 = Color.fromRGBO(253, 147, 64, 1.0);
+const Color mainColor90 = Color.fromRGBO(249, 138, 50, 1.0);
+const Color mainColor70 = Color.fromRGBO(254, 180, 122, 1.0);
+const Color mainColor50 = Color.fromRGBO(253, 200, 159, 1.0);
+const Color mainColor30 = Color.fromRGBO(255, 223, 198, 1.0);
+const Color mainColor10 = Color.fromRGBO(255, 245, 236, 1.0);
+
+// Black White
+const Color blackWhite100 = Color.fromRGBO(0, 0, 0, 1.0);
+const Color blackWhite90 = Color.fromRGBO(26, 25, 25, 1.0);
+const Color blackWhite70 = Color.fromRGBO(77, 76, 76, 1.0);
+const Color blackWhite50 = Color.fromRGBO(128, 127, 126, 1.0);
+const Color blackWhite30 = Color.fromRGBO(179, 177, 176, 1.0);
+const Color blackWhite10 = Color.fromRGBO(229, 228, 227, 1.0);
+const Color offWhite = Color.fromRGBO(255, 255, 255, 1.0);
+const Color offWhiteBackground = Color.fromRGBO(248, 248, 248, 1.0);
+
+// --------------
